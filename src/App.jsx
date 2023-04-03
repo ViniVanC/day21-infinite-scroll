e5ab3fb42773e588f6eaa88d5d409b7e2fa8cb6d
ViniVanC <vincentvan0302@gmail.com>
2023-04-03 18:57:23 +0300

    added Content to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Content } from "./components/Comtent/Content";
 import { Header } from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Content />
     </>
   );
 };
