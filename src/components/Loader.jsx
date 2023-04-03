2bb194253fc93da5e445bc70719fbb6dfa8a3cee
ViniVanC <vincentvan0302@gmail.com>
2023-04-03 19:24:46 +0300

    created Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Loader = () => {
+  return (
+    <div className="loader mx-auto">
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
