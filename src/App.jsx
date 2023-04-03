07cf7429f504cd50e0183369d727a655b5438a76
ViniVanC <vincentvan0302@gmail.com>
2023-04-03 19:25:16 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Content } from "./components/Comtent/Content";
+import { Content } from "./components/Content/Content";
 import { Header } from "./components/Header/Header";
 
 const App = () => {
