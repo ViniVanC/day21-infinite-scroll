e49c31c55cee9292686ae4e0054a55b5d9e5eaf7
ViniVanC <vincentvan0302@gmail.com>
2023-04-03 16:56:53 +0300

    added Header.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Header } from "./components/Header/Header";
 
 const App = () => {
   return (
-    <div className="flex h-[100vh] items-center justify-center bg-white text-black">
-      hi
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
